7f35778541a8d81fb62f3fc2d5f40b6f6fc8fa68
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-05-15 15:44:30 -0300

    feat: create first model and first migration

--- djangoapp/ToDo/models.py
+++ djangoapp/ToDo/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class ToDo(models.Model):
+  name = models.CharField("Nome da Tarefa", max_length=100)
+  description = models.TextField("Descrição da tarefa")
+  status = models.BooleanField("Status da tarefa")
+  created_at = models.DateTimeField("Data de Criação", auto_now=True)
+  updated_at = models.DateField("Data de atualização", auto_now=True)
+  
+  def __str__(self) -> str:
+    return self.name
